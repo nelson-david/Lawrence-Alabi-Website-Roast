a48b46c71cdae33c70ec0d7ded9659ec20be8a39
nelson-david <larva.code15@gmail.com>
2025-01-27 00:10:59 +0100

    Fixing smooth scroll issues on mobile 8

--- src/app/layouts/PageLayout.tsx
+++ src/app/layouts/PageLayout.tsx
@@ -2,51 +2,31 @@
 
 import { PropsWithChildren, useCallback, useEffect, useState } from "react";
 import Lenis from "@studio-freight/lenis";
-import AOS from "aos";
+// import AOS from "aos";
 import Footer from "../components/navigation/Footer";
 
 const PageLayout = ({ children }: PropsWithChildren) => {
-    // const [lenis, setLenis] = useState<Lenis | null>(null);
+    const [lenis, setLenis] = useState<Lenis | null>(null);
 
-    // function isMobile() {
-    //     return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-    //         navigator.userAgent
-    //     );
-    // }
-
-    // const raf = useCallback(
-    //     (time: number) => {
-    //         lenis?.raf(time);
-    //         requestAnimationFrame(raf);
-    //     },
-    //     [lenis]
-    // );
+    const raf = useCallback(
+        (time: number) => {
+            lenis?.raf(time);
+            requestAnimationFrame(raf);
+        },
+        [lenis]
+    );
 
     useEffect(() => {
-        AOS.init({
-            duration: 1000,
-        });
+        if (!lenis) {
+            setLenis(new Lenis());
+        }
 
-        // if (!lenis) {
-        //     if (!isMobile()) {
-        //         setLenis(
-        //             new Lenis({
-        //                 lerp: isMobile() ? 0.2 : 0.1, // Adjust lerp for mobile
-        //                 smoothWheel: !isMobile(), // Disable smooth wheel on mobile
-        //             })
-        //         );
+        requestAnimationFrame(raf);
 
-        //         requestAnimationFrame(raf);
-        //     } else {
-        //         // Fallback to native scrolling on mobile
-        //         document.documentElement.style.scrollBehavior = "smooth";
-        //     }
-        // }
-
-        // return () => {
-        //     lenis?.destroy();
-        // };
-    }, []);
+        return () => {
+            lenis?.destroy();
+        };
+    }, [lenis, raf]);
 
     return (
         <div className="pageLayout">
